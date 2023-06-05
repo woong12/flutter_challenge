2222a5a7d1e3e7f73bfb9cc7f9f4142547d240e1
woong <koohw0109@gmail.com>
2023-06-06 07:38:08 +0900

    setting

---
+++ schedule_ui/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Schedule());
+}
+
+class Schedule extends StatelessWidget {
+  const Schedule({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
